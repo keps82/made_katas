4be10b5dc909ce8b8cf7699b536f28daee8d3511
Kate Sugden <katikins@gmail.com>
2022-02-22 08:37:16 +0000

    number to LCD kata - part 2. Working. Ready to refactor.

--- number_to_lcd.py
+++ number_to_lcd.py
@@ -1,11 +1,84 @@
 # number to LCD
 
+UPRIGHT = '|'
+HORIZONTAL = '_'
+FILLER = ' '
+
+
+class LCD_line():
+
+
+    def __init__(self, first, middle, final):
+        self.first = first
+        self.middle = middle
+        self.final = final
+        
+
+
 class LCD_char():
 
-    def __init__(self, char, lcd_lines, fill=3):
+
+    def __init__(self, char, lcd_lines, scale=[1,1]):
         self.char = char
-        self.fill = fill
-        self.lcd_lines = lcd_lines
+        self.scale = scale
+        self.lcd_lines = self.scale_lines(lcd_lines, scale)
+
+
+    @staticmethod
+    def x_scale_lines(lcd_lines, x_scale):
+        """
+        >>> print_lcd_lines(LCD_char.x_scale_lines(['___'],3))
+        _____
+        """
+        scaled_lcd_lines = lcd_lines.copy()
+
+        for index,line in enumerate(lcd_lines):
+            x_scale_line = line[0] + line[1]*x_scale + line[2]
+            #print(x_scale_line)
+            scaled_lcd_lines[index] = x_scale_line
+
+        return scaled_lcd_lines
+
+
+    @staticmethod
+    def y_scale_lines(lcd_lines, y_scale):
+        """
+        >>> print_lcd_lines(LCD_char.y_scale_lines([' ','| |','|_|'],2))
+        <BLANKLINE>
+        | |
+        | |
+        | |
+        |_|
+        """
+        scaled_lcd_lines = lcd_lines.copy()
+
+        bottom_element_extension = [lcd_lines[2].replace(HORIZONTAL, FILLER)]*(y_scale-1) # ['','','']
+        middle_element_extension = [lcd_lines[1].replace(HORIZONTAL, FILLER)]*(y_scale-1)
+
+        scaled_lcd_lines = [lcd_lines[0]] + middle_element_extension + [lcd_lines[1]] + bottom_element_extension + [lcd_lines[2]]
+
+        return scaled_lcd_lines
+
+
+    @staticmethod
+    def scale_lines(lcd_lines, scale):
+        """
+        >>> print_lcd_lines(LCD_char.scale_lines([' _ ','| |','|_|'], [3,2]))
+         ___ 
+        |   |
+        |   |
+        |   |
+        |___|
+        """
+        x_scale_factor = scale[0]
+        y_scale_factor = scale[1]
+
+        scaled_lcd_lines = LCD_char.y_scale_lines(LCD_char.x_scale_lines(lcd_lines, x_scale_factor), y_scale_factor)
+#        scaled_lcd_lines = LCD_char.x_scale_lines(lcd_lines, x_scale_factor)
+
+        return scaled_lcd_lines
+
+
 
 
 one   = LCD_char('1', ['   ', '  |', '  |'])
@@ -17,13 +90,15 @@ six   = LCD_char('6', [' _ ', '|_ ', '|_|'])
 seven = LCD_char('7', [' _ ', '  |', '  |'])
 eight = LCD_char('8', [' _ ', '|_|', '|_|'])
 nine  = LCD_char('9', [' _ ', '|_|', ' _|'])
-zero  = LCD_char('0', [' _ ', '| |', ' _ '])
+zero  = LCD_char('0', [' _ ', '| |', '|_|'])
 
 
 digits = [one, two, three, four, five, six, seven, eight, nine, zero]
 
 
 digits_dict = {digit.char: digit.lcd_lines for digit in digits}
+
+
 
 
 def print_lcd_lines(lcd_lines):
